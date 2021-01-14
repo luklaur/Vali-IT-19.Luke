171396c299eb65ff91dd2bd50008ce5b0782f2d1
luklaur <lauri.luke3@gmail.com>
2021-01-15 01:35:30 +0200

    15.01 - Audacity class added

--- src/main/java/ee/bcs/valiit/tasks/Audacity.java
+++ src/main/java/ee/bcs/valiit/tasks/Audacity.java
@@ -2,12 +2,72 @@ package ee.bcs.valiit.tasks;
 
 public class Audacity {
 
+    public static void main(String[] args) {
+
+//        System.out.println(findLongestName(new String[]{"Jack", "Paul", "Larry", "Mary"}));
+//        System.out.println(searchForMax(new int[]{2, 4, 6, 8}));
+//        System.out.println(searchForMin(new int[]{2, 4, 6, 8}));
+//        System.out.println(average(new double[]{75, 73, 72, 80}));
 
 
+    }
 
+    private static String findLongestName(String[] names) {
+        //TODO AUDACITY harjutus
+        //leia massiivist pikim nimi
+        int size = names.length;
+        String longestName = names[0];
+        for (int i = 1; i < size; i++) {
+            // >= tiebreaker
+            if (names[i].length() >= longestName.length()) {
+                longestName = names[i];
+            }
+        }
+        return longestName;
+    }
 
+    private static int searchForMax(int[] numbers) {
+        // TODO AUDACITY harjutus
+        // leiab massiivist suurima numbri ja tagastab selle
+        int size = numbers.length;
+        int max = numbers[0];
+        for (int i = 1; i < size; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
 
+    private static int searchForMin(int[] numbers) {
+        // TODO AUDACITY harjutus
+        // leiab massiivist väikseima numbri ja tagastab selle
+        // ainuke erinevus eelmisega on < märk
+        int size = numbers.length;
+        int min = numbers[0];
+        for (int i = 1; i < size; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
 
+    private static double average(double[] numbers) {
+        // TODO AUDACITY harjutus
+        // arvuta massiivi keskmine
+        // kasuta double, et saada täpne tulemus
+        int size = numbers.length;
+        double total = 0;
+        for (int i = 0; i < size; i++) {
+            // liidab kõik arvud massiivis
+            // ja talletab need totalis
+            total += numbers[i];
+        }
+        // jagab summa massiivi pikkusega
+        double average = total / size;
+        return average;
+    }
 
 
 }
--- src/main/java/ee/bcs/valiit/tasks/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -4,72 +4,12 @@ package ee.bcs.valiit.tasks;
 public class Lesson3 {
     public static void main(String[] args) {
 
-//        System.out.println(findLongestName(new String[]{"Jack", "Paul", "Larry", "Mary"}));
-//        System.out.println(searchForMax(new int[]{2, 4, 6, 8}));
-//        System.out.println(searchForMin(new int[]{2, 4, 6, 8}));
-//        System.out.println(average(new double[]{75, 73, 72, 80}));
 //        System.out.println(sum(new int[]{2, 4, 6, 8, 10, 12, 14}));
 //        System.out.println(factorial(5));
 //        System.out.println(Arrays.toString(sort(new int[]{5, -6, 9, 12, 7})));
 //        System.out.println(reverseString("Your text here"));
 //        System.out.println(isPrime(7));
-    }
 
-    private static String findLongestName(String[] names) {
-        //TODO AUDACITY harjutus
-        //leia massiivist pikim nimi
-        int size = names.length;
-        String longestName = names[0];
-        for (int i = 1; i < size; i++) {
-            // >= tiebreaker
-            if (names[i].length() >= longestName.length()) {
-                longestName = names[i];
-            }
-        }
-        return longestName;
-    }
-
-    private static int searchForMax(int[] numbers) {
-        // TODO AUDACITY harjutus
-        // leiab massiivist suurima numbri ja tagastab selle
-        int size = numbers.length;
-        int max = numbers[0];
-        for (int i = 1; i < size; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-            }
-        }
-        return max;
-    }
-
-    private static int searchForMin(int[] numbers) {
-        // TODO AUDACITY harjutus
-        // leiab massiivist väikseima numbri ja tagastab selle
-        // ainuke erinevus eelmisega on < märk
-        int size = numbers.length;
-        int min = numbers[0];
-        for (int i = 1; i < size; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
-        }
-        return min;
-    }
-
-    private static double average(double[] numbers) {
-        // TODO AUDACITY harjutus
-        // arvuta massiivi keskmine
-        // kasuta double, et saada täpne tulemus
-        int size = numbers.length;
-        double total = 0;
-        for (int i = 0; i < size; i++) {
-            // liidab kõik arvud massiivis
-            // ja talletab need totalis
-            total += numbers[i];
-        }
-        // jagab summa massiivi pikkusega
-        double average = total / size;
-        return average;
     }
 
     public static int sum(int[] x) {
@@ -164,3 +104,4 @@ public class Lesson3 {
 
 
 }
+
