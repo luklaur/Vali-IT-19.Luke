082dff9f8aa4d689d61e40779d690f1075369fe4
luklaur <lauri.luke3@gmail.com>
2021-01-15 00:45:49 +0200

    15.01 - lesson3 - sortForMin added

--- src/main/java/ee/bcs/valiit/tasks/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        System.out.println(average(new double[]{75, 73, 72, 80}));
+
+        System.out.println(searchForMax(new int[]{2, 4, 6, 8}));
+//        System.out.println(searchForMin(new int[]{2, 4, 6, 8}));
+//        System.out.println(average(new double[]{75, 73, 72, 80}));
 //        System.out.println(sum(new int[]{2, 4, 6, 8, 10, 12, 14}));
 //        System.out.println(factorial(5));
 //        System.out.println(Arrays.toString(sort(new int[]{5, -6, 9, 12, 7})));
@@ -13,15 +16,35 @@ public class Lesson3 {
 //        System.out.println(isPrime(7));
     }
 
+    private static int searchForMax(int[] numbers) {
+
+    }
+
+    private static int searchForMin(int[] numbers) {
+        // TODO AUDACITY harjutus
+        // leiab massiivist väikseima numbri ja tagastab selle
+        int size = numbers.length;
+        int min = numbers[0];
+        for (int i = 1; i < size; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
     private static double average(double[] numbers) {
-        // TODO Audacity harjutus
+        // TODO AUDACITY harjutus
         // arvuta massiivi keskmine
         // kasuta double, et saada täpne tulemus
         int size = numbers.length;
         double total = 0;
         for (int i = 0; i < size; i++) {
+            // liidab kõik arvud massiivis
+            // ja talletab need totalis
             total += numbers[i];
         }
+        // jagab summa massiivi pikkusega
         double average = total / size;
         return average;
     }
