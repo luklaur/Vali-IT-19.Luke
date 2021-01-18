55efc66319e5f8c7d767181f55b6a893ac2167be
luklaur <lauri.luke3@gmail.com>
2021-01-18 14:08:09 +0200

    testController2

---
+++ src/main/java/ee/bcs/valiit/controller/MathUtilController2.java
@@ -0,0 +1,7 @@
+package ee.bcs.valiit.controller;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class MathUtilController2 {
+//    public int min(@RequestParam()
+}
--- src/main/java/ee/bcs/valiit/controller/TestController.java
+++ src/main/java/ee/bcs/valiit/controller/TestController.java
@@ -1,14 +1,15 @@
 package ee.bcs.valiit.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Generated;
 
 @RestController
 public class TestController {
-    @GetMapping("test")
-    public String getHelloWorld() {
-        return "Hello world!";
+    @GetMapping("test/{name}")
+    public String getHelloWorld(@PathVariable("name") String userName) {
+        return "Hello world! " + userName;
     }
 }
