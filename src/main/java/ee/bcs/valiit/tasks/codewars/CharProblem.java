39da286d50abd45dc969c4bf828a873f314a8168
luklaur <lauri.luke3@gmail.com>
2021-01-18 10:12:51 +0200

    Codewars 4 ülesannet valmis alustama lahendamist

---
+++ src/main/java/ee/bcs/valiit/tasks/codewars/CharProblem.java
@@ -0,0 +1,23 @@
+package ee.bcs.valiit.tasks.codewars;
+/*
+Ask a small girl - "How old are you?".
+She always says strange things... Lets help her!
+
+For correct answer program should return int from 0 to 9.
+
+Assume test input string always valid and may look
+like "1 year old" or "5 years old", etc..
+The first char is number only.
+*/
+
+
+public class CharProblem {
+    public static void main(String[] args) {
+
+        /*public static int howOld ( final String herOld){
+
+            //your code here, return correct age as int ; )
+
+        }*/
+    }
+}
