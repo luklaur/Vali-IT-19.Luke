88dc7387e50d7d23cb13557b5270d011fbc97cbd
luklaur <lauri.luke3@gmail.com>
2021-01-18 16:51:37 +0200

    MathUtilController, Lesson2Controller, vigane2

--- src/main/java/ee/bcs/valiit/controller/Lesson2Controller.java
+++ src/main/java/ee/bcs/valiit/controller/Lesson2Controller.java
@@ -7,23 +7,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Lesson2Controller {
 
-/*
     // http://localhost:8080/test/lauri
     @GetMapping("test/{name}")
-    public String getHelloWorld(@PathVariable("name") String userName) {
+    public String tere(@PathVariable("name") String userName) {
         return "Hello world! " + userName;
     }
-*/
 
     // http://localhost:8080/tasks/exercise4/5
     @GetMapping("exercise4/{b}")
     public int exercise4(@PathVariable("b") int bVariable) {
         return Lesson2.exercise4(bVariable);
     }
-  /*  // http://localhost:8080/tests/exercise5/3/6
-    @GetMapping("exercise5/{x}/{y}")
-    public String exercise5(@PathVariable("x") int xVariable, @PathVariable("y") int yVariable) {
-        return Lesson2.exercise5(xVariable, yVariable);
-    }*/
-
 }
--- src/main/java/ee/bcs/valiit/controller/MathUtilController.java
+++ src/main/java/ee/bcs/valiit/controller/MathUtilController.java
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("tasks")
 @RestController
 public class MathUtilController {
-    // http://localhost:8080/tasks/minOf2/5/6
-    @GetMapping("minOf2{a}/{b}")
+    // http://localhost:8080/tasks/minOf2?a=5&b=6
+    @GetMapping("minOf2")
     public int minOf2(@RequestParam("a") int aVariable, @RequestParam("b") int bVariable) {
         return Lesson1MathUtil.minOf2(aVariable, bVariable);
     }
 
-    // http://localhost:8080/tasks/minOf3/5/6/7
-    @GetMapping("minOf3{a}/{b}/{c}")
+    // http://localhost:8080/tasks/minOf3?a=5&b=6&c=7
+    @GetMapping("minOf3")
     public int minOf3(@RequestParam("a") int aVariable, @RequestParam("b") int bVariable, @RequestParam("c") int cVariable) {
         return Lesson1MathUtil.minOf3(aVariable, bVariable, cVariable);
     }
 
     // http://localhost:8080/tasks/maxOf2/5/6
     @GetMapping("maxOf2{a}/{b}")
-    public int maxOf2(@RequestParam("a") int aVariable, @RequestParam("b") int bVariable) {
+    public int maxOf2(@PathVariable("a") int aVariable, @PathVariable("b") int bVariable) {
         return Lesson1MathUtil.maxOf2(aVariable, bVariable);
     }
 
     // http://localhost:8080/tasks/maxOf3/5/6/7
     @GetMapping("maxOf3{a}/{b}/{c}")
-    public int maxOf3(@RequestParam("a") int aVariable, @RequestParam("b") int bVariable, @RequestParam("c") int cVariable) {
+    public int maxOf3(@PathVariable("a") int aVariable, @PathVariable("b") int bVariable, @PathVariable("c") int cVariable) {
         return Lesson1MathUtil.maxOf3(aVariable, bVariable, cVariable);
     }
 
