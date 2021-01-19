d14296c04b440f3c65ec523b12e0e731c119b87b
luklaur <lauri.luke3@gmail.com>
2021-01-19 15:49:27 +0200

    Lesson2 ex5 controller töötab

--- src/main/java/ee/bcs/valiit/controller/Lesson2Controller.java
+++ src/main/java/ee/bcs/valiit/controller/Lesson2Controller.java
@@ -18,7 +18,7 @@ public class Lesson2Controller {
 
     // http://localhost:8080/tasks/exercise1?array=10,9,8,7,6,5,4
     @GetMapping("exercise1")
-    public int[] exercise1(@RequestParam("array") int [] array) {
+    public int[] exercise1(@RequestParam("array") int[] array) {
         return Lesson2.exercise1(array);
     }
 
@@ -28,11 +28,16 @@ public class Lesson2Controller {
         return Lesson2.exercise2(x);
     }
 
-
-
     // http://localhost:8080/tasks/exercise4/5
     @GetMapping("exercise4/{b}")
     public int exercise4(@PathVariable("b") int bVariable) {
         return Lesson2.exercise4(bVariable);
+    }
+
+    // http://localhost:8080/tasks/exercise5?x=5&y=4
+    @GetMapping("exercise5")
+    public String exercise5(@RequestParam("x") int xVariable,
+                            @RequestParam("y") int yVariable) {
+        return Lesson2.exercise5(xVariable, yVariable);
     }
 }
--- src/main/java/ee/bcs/valiit/tasks/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -6,7 +6,7 @@ public class Lesson3 {
 //        System.out.println(sum(new int[]{2, 4, 6, 8, 10, 12, 14}));
 //        System.out.println(factorial(5));
 //        System.out.println(Arrays.toString(sort(new int[]{5, -6, 9, 12, 7})));
-        System.out.println(reverseString("Your text here"));
+//        System.out.println(reverseString("Your text here"));
 //        System.out.println(isPrime(17));
     }
 
