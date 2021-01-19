aaf7a5fe613f4a88c71afc83704b3775bc5512d7
luklaur <lauri.luke3@gmail.com>
2021-01-19 23:28:53 +0200

    Lesson3HardController evenFibonacci töötab

--- src/main/java/ee/bcs/valiit/controller/Lesson3HardController.java
+++ src/main/java/ee/bcs/valiit/controller/Lesson3HardController.java
@@ -1,4 +1,15 @@
 package ee.bcs.valiit.controller;
 
+import ee.bcs.valiit.tasks.Lesson3;
+import ee.bcs.valiit.tasks.Lesson3Hard;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("tasks")
+@RestController
 public class Lesson3HardController {
+    // http://localhost:8080/tasks/evenFibonacci?x=10
+    @GetMapping("evenFibonacci")
+    public int evenFibonacci(@RequestParam("x") int x) {
+        return Lesson3Hard.evenFibonacci(x);
+    }
 }
