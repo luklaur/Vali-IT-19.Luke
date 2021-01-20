1970fb60ecb0e3cd0c918e52cdbf8d95dccde697
luklaur <lauri.luke3@gmail.com>
2021-01-20 16:18:59 +0200

    BookController töötab, raamatute array list töötab ka läbi Postmani

--- src/main/java/ee/bcs/valiit/controller/BookController.java
+++ src/main/java/ee/bcs/valiit/controller/BookController.java
@@ -5,10 +5,49 @@ import ee.bcs.valiit.tasks.Book;
 import ee.bcs.valiit.tasks.Lesson2;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @RequestMapping("tasks")
 @RestController
 public class BookController {
-    // http://localhost:8080/tasks
+
+//    HashMap<String, String> raamatud = new HashMap<>();
+
+    private List<Book> raamatud = new ArrayList<>();
+
+    // RequestBody????
+    // http://localhost:8080/tasks/addBook
+    @PostMapping("addBook")
+    public void addBook(@RequestBody Book book) {
+        raamatud.add(book);
+    }
+
+    // http://localhost:8080/tasks/viewBook?all
+    @GetMapping("viewBook")
+    public List<Book> viewBook(@RequestParam("all") String all) {
+        return raamatud;
+    }
+
+    // http://localhost:8080/tasks/viewBook/3
+    @GetMapping("viewBook/{ID}")
+    public Book viewBook(@PathVariable("ID") int ID) {
+        return raamatud.get(ID);
+    }
+
+    // http://localhost:8080/tasks/replaceBook/2
+    @PutMapping("replaceBook/{ID}")
+    public void replaceBook(@RequestBody Book book, @PathVariable("ID") int ID) {
+        raamatud.set(ID, book);
+    }
+
+    // http://localhost:8080/tasks/deleteBook/2
+    @DeleteMapping("deleteBook/{ID}")
+    public void deleteBook(@PathVariable("ID") int ID) {
+        raamatud.remove(ID);
+    }
+
 
     // http://localhost:8080/tasks/Book?title=Ulmeraamat&authors=Priit&yearOfRelease=2020&nrOfPages=666
     @GetMapping("Book")
