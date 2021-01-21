a3a6dcb74a4c362d97e2894cf28d58f4ca3c528a
luklaur <lauri.luke3@gmail.com>
2021-01-21 14:30:00 +0200

    RandomGame controller algus

--- src/main/java/ee/bcs/valiit/controller/RandomGameController.java
+++ src/main/java/ee/bcs/valiit/controller/RandomGameController.java
@@ -1,4 +1,41 @@
 package ee.bcs.valiit.controller;
 
+import ee.bcs.valiit.tasks.Lesson3;
+import ee.bcs.valiit.tasks.RandomGame;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("tasks")
+@RestController
 public class RandomGameController {
+
+    // http://localhost:8080/tasks/setGuess
+    @GetMapping("reverseString/{setGuess}")
+    public String setGuess(@PathVariable("setGuess") String numbersGame) {
+        return "Sisesta number";
+/*
+
+        int count = 0;
+        for (int i = 10; i > 0; i--){
+            count++;
+            int guess = scanner.nextInt();
+            if (guess == randomNumber) {
+                System.out.println("Arvasid ära " + count + " korraga");
+                break;
+            } else if(count >= 10){
+                System.out.println("Ei arvanud ära. Tegelik number oli: " + randomNumber);
+                break;
+            } else if (guess > randomNumber) {
+                System.out.println("Number oli väiksem");
+            } else {
+                System.out.println("Number oli suurem");
+            }
+        }
+
+
+*/
+
+    }
 }
--- src/main/java/ee/bcs/valiit/tasks/RandomGame.java
+++ src/main/java/ee/bcs/valiit/tasks/RandomGame.java
@@ -1,5 +1,49 @@
 package ee.bcs.valiit.tasks;
 
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
 public class RandomGame {
+
+    Random random = new Random();
     int randomNumber = random.nextInt(100);
+    Scanner scanner = new Scanner(System.in);
+    int guess = scanner.nextInt();
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
 }
+
+
+
+
