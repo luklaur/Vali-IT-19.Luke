d2c94539d98c1558dc51082dadd557290a97f165
luklaur <lauri.luke3@gmail.com>
2021-01-24 23:18:24 +0200

    BankController transferMoney töötab

--- src/main/java/ee/bcs/valiit/tasks/BankController.java
+++ src/main/java/ee/bcs/valiit/tasks/BankController.java
@@ -80,10 +80,40 @@ public class BankController {
 
     }
 
-    // http://localhost:8080/solution/bank/transferMoney?fromAccount=EE123&toAccount=EE124&amount=12
-//    public void transferMoney() {
+    // http://localhost:8080/tasks/transferMoney?fromAccount=EE748&toAccount=EE124&amount=100
+    @GetMapping("transferMoney")
+    public void transferMoney(@RequestParam("fromAccount") String fromAccount,
+                              @RequestParam("toAccount") String toAccount,
+                              @RequestParam("amount") BigDecimal amount){
+        String sql = "SELECT balance FROM account where account_nr = :account_nr";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("account_nr", fromAccount);
+        BigDecimal fromAccountBalance = jdbcTemplate.queryForObject(sql, paramMap, BigDecimal.class);
 
-//    }
+        BigDecimal newFromAccountBalance = fromAccountBalance.subtract(amount);
+        if(newFromAccountBalance.compareTo(BigDecimal.ZERO) < 0){
+            throw new RuntimeException("Not enough money");
+        }
+
+        String sql2 = "UPDATE account SET balance = :balance WHERE account_nr = :account_nr";
+        Map<String, Object> paramMap2 = new HashMap();
+        paramMap2.put("account_nr", fromAccount);
+        paramMap2.put("balance", newFromAccountBalance);
+        jdbcTemplate.update(sql2, paramMap2);
+
+        String sql3 = "SELECT balance FROM account where account_nr = :account_nr";
+        Map<String, Object> paramMap3 = new HashMap<>();
+        paramMap3.put("account_nr", toAccount);
+        BigDecimal toAccountBalance = jdbcTemplate.queryForObject(sql3, paramMap3, BigDecimal.class);
+
+        BigDecimal newToAccountBalance = toAccountBalance.add(amount);
+
+        String sql4 = "UPDATE account SET balance = :balance WHERE account_nr = :account_nr";
+        Map<String, Object> paramMap4 = new HashMap();
+        paramMap4.put("account_nr", toAccount);
+        paramMap4.put("balance", newToAccountBalance);
+        jdbcTemplate.update(sql4, paramMap4);
+    }
 
 
 }
